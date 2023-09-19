213166d9a7760770eabbe3d0f8c279a4e5d27224
Eakteker jahan <eaktekerjahan@gmail.com>
2023-09-19 15:56:46 +0600

    user details

--- src/main/java/com/example/smartdiagnosissystem/repository/UserRepository.java
+++ src/main/java/com/example/smartdiagnosissystem/repository/UserRepository.java
@@ -1,7 +1,8 @@
 package com.example.smartdiagnosissystem.repository;
 
+
 import com.example.smartdiagnosissystem.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User,Long> {
 }
