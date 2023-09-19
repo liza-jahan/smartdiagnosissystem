aa104a3d42b195bc47a81ff79462a3636bda4212
Eakteker jahan <eaktekerjahan@gmail.com>
2023-09-19 15:04:30 +0600

    Initial commit

---
+++ src/main/java/com/example/smartdiagnosissystem/SmartdiagnosissystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.smartdiagnosissystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartdiagnosissystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartdiagnosissystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/smartdiagnosissystem/SmartdiagnosissystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.smartdiagnosissystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartdiagnosissystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
