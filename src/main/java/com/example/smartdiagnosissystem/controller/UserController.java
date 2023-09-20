14c792b026c74641cddc5da234444f26879d309d
Eakteker jahan <eaktekerjahan@gmail.com>
2023-09-20 23:27:19 +0600

    React Start.create Registration Page

--- src/main/java/com/example/smartdiagnosissystem/controller/UserController.java
+++ src/main/java/com/example/smartdiagnosissystem/controller/UserController.java
@@ -1,9 +1,25 @@
 package com.example.smartdiagnosissystem.controller;
 
+import com.example.smartdiagnosissystem.model.User;
+import com.example.smartdiagnosissystem.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequestMapping("/api/v1/")
 public class UserController {
 
+@Autowired
+private UserRepository userRepository;
 
+//Get All users
+   @GetMapping("/users")
+   public List<User>  getAllUser()
+    {
+        return  userRepository.findAll();
+    }
 }
