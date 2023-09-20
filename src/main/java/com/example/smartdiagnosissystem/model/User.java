673955c8195a514bec9679bf2276635d1b8dfde5
Eakteker jahan <eaktekerjahan@gmail.com>
2023-09-20 17:53:11 +0600

    React Start.create Registration Page

--- src/main/java/com/example/smartdiagnosissystem/model/User.java
+++ src/main/java/com/example/smartdiagnosissystem/model/User.java
@@ -1,10 +1,7 @@
 package com.example.smartdiagnosissystem.model;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,13 +12,18 @@ import lombok.Setter;
     @Setter
     @AllArgsConstructor
     @NoArgsConstructor
+    @Table(name="userinfo")
     public class User {
         @Id
         @GeneratedValue(strategy = GenerationType.IDENTITY)
         public  long id;
+        @Column(nullable = false,length = 20)
         public  String userName;
+        @Column(nullable = false, unique = true, length = 45)
         public  String email;
+        @Column(nullable = false,length =30)
         public  String password;
+        @Column(nullable = false,length = 56,unique = true)
         public  int phoneNumber;
 
 
