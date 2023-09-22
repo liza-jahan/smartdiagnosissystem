d58896eabc600a83dc1ccf3ac906dc2248453ad3
Eakteker jahan <eaktekerjahan@gmail.com>
2023-09-23 01:11:08 +0600

    UserService and UserRepository Update

--- src/main/java/com/example/smartdiagnosissystem/controller/UserController.java
+++ src/main/java/com/example/smartdiagnosissystem/controller/UserController.java
@@ -1,25 +1,22 @@
 package com.example.smartdiagnosissystem.controller;
 
 import com.example.smartdiagnosissystem.model.User;
-import com.example.smartdiagnosissystem.repository.UserRepository;
+import com.example.smartdiagnosissystem.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/")
 public class UserController {
 
 @Autowired
-private UserRepository userRepository;
+private UserService userService;
 
 //Get All users
-   @GetMapping("/users")
-   public String saveUser()
+   @PostMapping("/Save")
+   public long saveUser(@RequestBody User user)
     {
-
+      userService.save(user);
+      return  user.getId();
     }
 }
