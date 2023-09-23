3860075779ae056af82fd6a8befe73515d330cb8
Eakteker jahan <eaktekerjahan@gmail.com>
2023-09-23 13:08:54 +0600

    Cors connection create.

--- src/main/java/com/example/smartdiagnosissystem/SmartdiagnosissystemApplication.java
+++ src/main/java/com/example/smartdiagnosissystem/SmartdiagnosissystemApplication.java
@@ -2,6 +2,9 @@ package com.example.smartdiagnosissystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class SmartdiagnosissystemApplication {
@@ -10,4 +13,19 @@ public class SmartdiagnosissystemApplication {
         SpringApplication.run(SmartdiagnosissystemApplication.class, args);
     }
 
+
+
+
+@Bean
+public WebMvcConfigurer corsConfigurer(){
+    return new WebMvcConfigurer() {
+        public void addCorsMapping(CorsRegistry corsRegistry){
+            corsRegistry.addMapping("/**")
+                    .allowedMethods("*")
+                    .allowedOrigins("http://localhost:3000");
+
+        }
+    };
+
+}
 }
