ca0c212bc802e953cf78b8e9618269198dc2361e
Eakteker jahan <eaktekerjahan@gmail.com>
2023-09-26 15:21:56 +0600

    save and get  user

--- src/main/java/com/example/smartdiagnosissystem/controller/UserController.java
+++ src/main/java/com/example/smartdiagnosissystem/controller/UserController.java
@@ -5,6 +5,8 @@ import com.example.smartdiagnosissystem.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/v1/")
 public class UserController {
@@ -12,11 +14,19 @@ public class UserController {
 @Autowired
 private UserService userService;
 
-//Get All users
-   @PostMapping("/Save")
+
+  @GetMapping("/getAll")
+  public List<User> getAllUser(){
+        return userService.getAllUser();
+    }
+
+//Save All users
+   @PostMapping("/save")
    public long saveUser(@RequestBody User user)
     {
       userService.save(user);
       return  user.getId();
     }
+
+
 }
--- src/main/java/com/example/smartdiagnosissystem/service/UserService.java
+++ src/main/java/com/example/smartdiagnosissystem/service/UserService.java
@@ -5,6 +5,8 @@ import com.example.smartdiagnosissystem.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class UserService {
 
@@ -16,4 +18,9 @@ public class UserService {
    }
 
 
+
+   public List<User> getAllUser()
+    {
+        return userRepository.findAll();
+    }
 }
